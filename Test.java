cbe3185c0629587b07ccb848ba7cb049b1114947
Lena <lena.weess@thg-noe.net>
2022-03-11 09:31:01 +0100

    Initial sharing of project

---
+++ Test.java
@@ -0,0 +1,33 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Test.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Test
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse Test
+     */
+    public Test()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
