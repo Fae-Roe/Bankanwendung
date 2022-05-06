45324d237a98df7456451e94fb86ebebf49feb2f
Angelina <Angelinapilkenrodt@gmail.com>
2022-05-06 09:41:05 +0200

    :

--- DATENBANKVERBINDUNG.java
+++ DATENBANKVERBINDUNG.java
@@ -81,8 +81,9 @@ public class DATENBANKVERBINDUNG
         return pin;
     }
 
-    public void PINAendern(String benutzername, int pNeu)
+    public void PINaendern(String benutzername, int pNeu)
     {
         db.conExecute("UPDATE MUSIK SET `PIN` = '" + pNeu + "' WHERE Name = '" + benutzername + "'"); 
     }
+    
 }
