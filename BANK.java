bad20d686d5843c765090b0034a58e71f484ca17
Lena <lena.weess@thg-noe.net>
2022-05-06 10:19:45 +0200

    neu

--- BANK.java
+++ BANK.java
@@ -13,4 +13,88 @@ public class BANK
         
     }
 
+    public int SparkontoEinrichten(double Kontostand, KUNDE kunde)
+    {
+        
+        return 0;
+    }
+    
+    public int GirokontoEinrichten(double Kontostand, KUNDE kunde)
+    {
+        
+        return 0;
+    }
+    
+    public boolean KontoLoeschen(KONTO konto)
+    {
+        
+        return true;
+    }
+    
+    public void AngestelltenLoeschen(ANGESTELLTER angestellter)
+    {
+        
+    }
+    
+    public KUNDE KundeSuchen(String name)
+    {
+        
+        return null;
+    }
+    
+    public int KontonummerFuerKundenGeben(KUNDE kunde)
+    {
+        
+        return 0;
+    }
+    
+    public ANGESTELLTER AngestelltenSuchen(String name)
+    {
+        
+        return null;
+    }
+    
+    public String[] KundennamenGeben()
+    {
+        
+        return null;
+    }
+    
+    public String[] AngestelltennamenGeben(ANGESTELLTER angestellter)
+    {
+        
+        return null;
+    }
+    
+    public void NeuenKundenEinrichten(String name, int pin)
+    {
+        
+    }
+    
+    public void NeuenKundenLoeschen(KUNDE kunde)
+    {
+        
+    }
+    
+    public void NeuenAngestelltenEinrichten(String name, int pin)
+    {
+        
+    }
+    
+    public BANK BankGeben()
+    {
+        
+        return null;
+    }
+    
+    public KONTO KontoSuchen(int pin)
+    {
+        
+        return null;
+    }
+    
+    public void Beenden()
+    {
+        
+    }
 }
