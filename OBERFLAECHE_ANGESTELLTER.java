f07f6180c3cc5248db7a695ff7b7cf12a6838901
Maja <maja.schlecht@thg-noe.net>
2022-05-20 09:32:17 +0200

    zz

--- OBERFLAECHE_ANGESTELLTER.java
+++ OBERFLAECHE_ANGESTELLTER.java
@@ -5,7 +5,6 @@
  * @author (Ihr Name) 
  * @version (eine Versionsnummer oder ein Datum)
  */
-<<<<<<< HEAD
 public class OBERFLAECHE_ANGESTELLTER extends OBERFLAECHE_LOGIN
 {
 
@@ -25,32 +24,6 @@ public class OBERFLAECHE_ANGESTELLTER extends OBERFLAECHE_LOGIN
      */
     public int beispielMethode(int y)
     {
-        // tragen Sie hier den Code ein
-=======
-public class OBERFLAECHE_ANGESTELLTER
-{
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse OBERFLAECHE_ANGESTELLTER
-     */
-    public OBERFLAECHE_ANGESTELLTER(int f, DATENBANKVERBINDUNG h)
-    {
-        // Instanzvariable initialisieren
-        x = 0;
     }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
->>>>>>> 95fbf972c23054f752530cf811a0081b4c817943
-    }
+    
 }
--- OBERFLAECHE_KUNDE.java
+++ OBERFLAECHE_KUNDE.java
@@ -5,7 +5,6 @@
  * @author (Ihr Name) 
  * @version (eine Versionsnummer oder ein Datum)
  */
-<<<<<<< HEAD
 public class OBERFLAECHE_KUNDE extends OBERFLAECHE_LOGIN
 {
     // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
@@ -17,31 +16,5 @@ public class OBERFLAECHE_KUNDE extends OBERFLAECHE_LOGIN
     public OBERFLAECHE_KUNDE()
     {
         super();
-=======
-public class OBERFLAECHE_KUNDE
-{
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse OBERFLAECHE_KUNDE
-     */
-    public OBERFLAECHE_KUNDE(int s, DATENBANKVERBINDUNG e)
-    {
-        // Instanzvariable initialisieren
-        x = 0;
->>>>>>> 95fbf972c23054f752530cf811a0081b4c817943
-    }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
     }
 }
