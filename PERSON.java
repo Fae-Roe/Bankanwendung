1c51272eecfcae38912fa59741f0fa830fbca43b
Jasmin <jasmin.geyer@thg-noe.net>
2022-05-30 10:16:55 +0200

    .

--- PERSON.java
+++ PERSON.java
@@ -10,13 +10,8 @@ public abstract class PERSON
     protected String name;
     protected int pin;
 
+    
     public PERSON(String n, int p)
-
-    public String name;
-    public int pin;
-    DATENBANKVERBINDUNG db;
-    public int Kundennummer;
-    public PERSON(String newname, int newpin)
 
     {
         name = n;
